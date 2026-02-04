aa8b8fae24bcfaa9d6ffea3e42f29d5b437068eb
Katharina Emilie Borg Baardseth <kebaards@stud.ntnu.no>
2026-02-04 14:35:51 +0100

    feat: install firebase, update json dependencies, and create admin and client.ts

---
+++ utleiometer/src/lib/firebase/client.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { env } from "../env"
+
+const firebaseConfig = {
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+}
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
